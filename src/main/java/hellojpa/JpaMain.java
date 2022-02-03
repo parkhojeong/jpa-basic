25e92ba29c71581308ef5bdce0e0b6250f76d853
parkhojeong <parkhj062@gmail.com>
2022-02-03 22:55:26 +0900

    영속성 컨텍스트 > 저장후 1차 캐시 조회 테스트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,7 +16,20 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // add to work
+            // 비영속
+            Member member = new Member();
+            member.setId(101L);
+            member.setName("HelloJPA");
+            
+            // 영속
+            System.out.println("=== BEFORE ===");
+            em.persist(member);
+            System.out.println("=== AFTER ===");
+
+            Member findMember = em.find(Member.class, 101L);
+
+            System.out.println("findMember.getId() = " + findMember.getId());
+            System.out.println("findMember.getName() = " + findMember.getName());
 
             tx.commit();
         } catch (Exception e) {
